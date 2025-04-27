e3fd1db8044e295dea6676a61645b99747c86a72
Owner <Owner@Priscilla-MBP.hitronhub.home>
2025-04-27 17:20:15 -0400

    Retry triggering workflow after settings fix

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,5 +2,5 @@ import json
 def lamdda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello updated Lambda from vscode ')
+        'body': json.dumps('Hello second updated Lambda from vscode ')
     }
