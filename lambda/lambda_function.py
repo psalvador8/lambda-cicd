9c2f4e2c232e6851cd08c3522190987e6789ae0c
Owner <Owner@Priscilla-MBP.hitronhub.home>
2025-04-27 16:11:33 -0400

    updates

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,5 +2,5 @@ import json
 def lamdda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from  our CICD gitbub actions workflow vscode')
+        'body': json.dumps('Hello updated Lambda from vscode')
     }
